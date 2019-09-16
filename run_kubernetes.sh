08ada3fcc9b25a61c0dff48927294fd28826d282
josephmfaulkner <josephmfaulkner@gmail.com>
2019-09-15 19:35:01 -0700

    kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=josephmfaulkner/udacitydevops-project4-sklearn
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --generator=run-pod/v1 sklearnapp --image=josephmfaulkner/udacitydevops-project4-sklearn:sklearnapp --port=5000
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward sklearnapp 5000:5000
+
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=josephmfaulkner/udacitydevops-project4-sklearn
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=josephmfaulkner
+docker tag sklearnapp:sklearnapp josephmfaulkner/udacitydevops-project4-sklearn:sklearnapp
 
 # Step 3:
 # Push image to a docker repository
+docker push josephmfaulkner/udacitydevops-project4-sklearn:sklearnapp
